fcc459537b08d7635472924c10bb7cdc08b72f0b
Soloviy <80028037+Soloviy@users.noreply.github.com>
2022-01-15 14:05:58 +0200

    HelloWorldGit

---
+++ HelloWorldGit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace HelloWorldGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "store.txt";
+            StreamReader reader = new StreamReader(path);
+            string line = "";
+
+            if (!reader.EndOfStream)
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл пустий");
+            }
+
+            reader.Close();
+
+            StreamWriter writer = new StreamWriter(path);
+            writer.WriteLine(DateTime.Now.ToString());
+            writer.Close();
+        }
+    }
+}
